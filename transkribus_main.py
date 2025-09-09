a9faf029966acdc533cc5042c82a35c752e47830
Bastian.2000 <162580964+BastianD2000@users.noreply.github.com>
2025-09-09 17:52:53 +0200

    Final version transkribus_main

--- transkribus_main.py
+++ transkribus_main.py
@@ -11,7 +11,8 @@ from transkribus_tasks import (
     export_and_download,
     upload_all_documents,
     upload_to_transkribus_via_ftp,
-    filter_new_documents
+    filter_new_documents,
+    wait_for_documents_to_appear
 )
 
 """
@@ -31,8 +32,13 @@ def login():
 def upload_documents_task(session_id, collection_id, local_upload_path):
     ftp_username = os.getenv("TRANSKRIBUS_EMAIL")
     ftp_password = os.getenv("TRANSKRIBUS_PASSWORD")
-    #upload_to_transkribus_via_ftp(session_id, ftp_username, ftp_password, collection_id, local_upload_path)
-    upload_all_documents(session_id, collection_id, local_upload_path)
+    upload_to_transkribus_via_ftp(session_id, ftp_username, ftp_password, collection_id, local_upload_path)
+    #upload_all_documents(session_id, collection_id, local_upload_path)
+    return upload_all_documents(session_id, collection_id, local_upload_path)
+
+@task
+def wait_for_documents_to_appear_task(session_id, collection_id, expected_titles):
+    return wait_for_documents_to_appear(session_id, collection_id, expected_titles)
 
 @task
 def filter_new_docs_task(session_id, col_id, doc_ids):
@@ -77,33 +83,35 @@ def transkribus_workflow():
     #     title="Transkribus Flow started",
     #     description="Workflow with upload and complete processing."
     # )
-    issue_id = None  # Dummy-Wert
+    issue_id = None  # None in case issues aren't used
 
     try:
         session_id = login()
 
         collection_id_for_upload = 1992893
-        local_upload_path = "upload"  # Oder absoluten Pfad angeben
-        upload_documents_task(session_id, collection_id_for_upload, local_upload_path)
+        local_upload_path = "upload"  
+        uploaded_titles = upload_documents_task(session_id, collection_id_for_upload, local_upload_path)
 
-        wait_for_completion(session_id, None)  # Warten auf Upload-Prozess (UploadJob)
-
+        wait_for_completion(session_id, None)  # wait for upload process
+        wait_for_documents_to_appear_task(session_id, collection_id_for_upload, uploaded_titles)
         collections = fetch_collections(session_id)
         for col_id, col_name in collections:
             documents = fetch_documents(session_id, col_id)
             all_doc_ids = [doc.get("docId") for doc in documents]
             new_doc_ids = filter_new_docs_task(session_id, col_id, all_doc_ids)
+            print(new_doc_ids)
             for doc_id in new_doc_ids:
                 page_ids = fetch_page_ids(session_id, col_id, doc_id)
                 analyze_layout(session_id, col_id, doc_id, page_ids)
-                wait_for_completion(session_id, doc_id)  # Warten auf LAJob
+                wait_for_completion(session_id, doc_id)  # wait for lajob
+                perform_ocr(session_id, col_id, doc_id, page_ids)
+                wait_for_completion(session_id, doc_id)  # wait for ocr
                 export_doc(session_id, col_id, doc_id)
 
-
-        # close_gitlab_issue(issue_id, success_message="Workflow erfolgreich abgeschlossen. Alle Dokumente verarbeitet.")
+        # close_gitlab_issue(issue_id, success_message="Workflow concluded successfully. All documents processed.")
 
     except Exception as e:
-        # update_gitlab_issue(issue_id, f"Fehler im Workflow: {str(e)}")
+        # update_gitlab_issue(issue_id, f"Error in workflow: {str(e)}")
         raise
 
 if __name__ == "__main__":
